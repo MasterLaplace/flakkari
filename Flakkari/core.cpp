1d242c50e51b13d3ad1b1a753332319cc45a692d
MasterLaplace <guillaume.papineau@epitech.eu>
2023-11-28 04:08:17 +0100

    feat(Flakkari): fix incrementator and add cmake to build

---
+++ Flakkari/core.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-REN-5-2-rtype-mathys.thevenot
+** File description:
+** core
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Server: Hello, World!" << endl;
+    return 0;
+}
