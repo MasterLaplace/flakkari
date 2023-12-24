ad7a0260909760f4f511343830aae3fbcf45e5c0
Master_Laplace <guillaume.papineau@epitech.eu>
2023-12-24 03:30:05 +0100

    feat(examples): Add UDPClient class for basic UDP client functionality

--- Flakkari/core.cpp
+++ Flakkari/core.cpp
@@ -5,9 +5,18 @@
 ** core
 */
 
+#if defined(CLIENT_MODE)
+#include "examples/UDPClient.hpp"
+#else
 #include "examples/UDPServer.hpp"
+#endif
 
 int main() {
+#if defined(CLIENT_MODE)
+    Flakkari::UDPClient client("localhost", 8080);
+    return client.run();
+#else
     Flakkari::UDPServer server("localhost", 8080);
     return server.run();
+#endif
 }
