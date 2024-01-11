a470dae1a447447efe4e37b99cbae78779b0ffb4
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-11 03:10:42 +0100

    update(Protocol): Refactor packet content handling

--- Flakkari/Protocol/Packet.cpp
+++ Flakkari/Protocol/Packet.cpp
@@ -15,31 +15,33 @@ namespace Flakkari::Protocol::API {
 
         Packet::Packet(Network::Buffer data) {
             header = Header(data);
-            content = data.extractData(sizeof(header), header._contentLength);
+            payload = data.extractData(sizeof(header), header._contentLength);
         }
 
-        Packet::Packet(Header header, Network::Buffer content)
-            : header(header), content(content) {}
+        Packet::Packet(Header header, Network::Buffer payload)
+            : header(header), payload(payload) {}
 
         Packet::Packet(Header header) : header(header) {}
 
         template<typename T>
-        void Packet::addContent(T content) {
-            this->content += Network::Buffer(content);
+        void Packet::addContent(T payload) {
+            this->payload += Network::Buffer(payload);
+            this->header._contentLength += sizeof(T);
         }
 
         void Packet::addFragment(Network::Buffer fragment) {
-            this->content += fragment;
+            this->payload += fragment;
             this->header._contentLength += fragment.size();
         }
 
         template<typename T>
-        void Packet::deleteContent(T content) {
-            this->content -= Network::Buffer(content);
+        void Packet::deleteContent(T payload) {
+            this->payload -= Network::Buffer(payload);
+            this->header._contentLength -= sizeof(T);
         }
 
         void Packet::deleteFragment(Network::Buffer fragment) {
-            this->content -= fragment;
+            this->payload -= fragment;
             this->header._contentLength -= fragment.size();
         }
 
@@ -47,26 +49,26 @@ namespace Flakkari::Protocol::API {
             std::copy((byte *) &header, (byte *) &header + sizeof(Header), buffer.begin());
         }
 
-        void serializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2) {
-            std::copy(buffer.begin(), buffer.end(), buffer2.begin());
+        void serializeBuffer(Network::Buffer buffer, Network::Buffer& dest) {
+            std::copy(buffer.begin(), buffer.end(), dest.begin());
         }
 
         void serializePacket(Packet packet, Network::Buffer& buffer) {
             serializeHeader(packet.header, buffer);
-            serializeBuffer(packet.content, buffer);
+            serializeBuffer(packet.payload, buffer);
         }
 
         void deserializeHeader(Network::Buffer buffer, Header& header) {
             std::copy(buffer.begin(), buffer.begin() + sizeof(Header), (byte *) &header);
         }
 
-        void deserializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2) {
-            std::copy(buffer.begin(), buffer.end(), buffer2.begin());
+        void deserializeBuffer(Network::Buffer buffer, Network::Buffer& dest) {
+            std::copy(buffer.begin(), buffer.end(), dest.begin());
         }
 
         void deserializePacket(Network::Buffer buffer, Packet& packet) {
             deserializeHeader(buffer, packet.header);
-            deserializeBuffer(buffer, packet.content);
+            deserializeBuffer(buffer, packet.payload);
         }
 
     } /* namespace V_1 */
--- Flakkari/Protocol/Packet.hpp
+++ Flakkari/Protocol/Packet.hpp
@@ -4,9 +4,9 @@
  * Flakkari Library is a C++ Library for Network.
  * @file Packet.hpp
  * @brief Flakkari::Protocol::API::Packet class header. This class is used to
- * represent a packet. A packet is a header and a content.
+ * represent a packet. A packet is a header and a payload.
  *  - The header is a Flakkari::Protocol::API::Header object.
- *  - The content is a Flakkari::Network::Buffer object.
+ *  - The payload is a Flakkari::Network::Buffer object.
  *
  * @see Flakkari::Protocol::API::Header
  * @see Flakkari::Network::Buffer
@@ -25,73 +25,73 @@
 
 namespace Flakkari::Protocol::API {
 
-    inline namespace V_1 {
+inline namespace V_1 {
 
     PACKED_START
 
+    /**
+     * @brief Flakkari Packet v1 (new packet)
+     *
+     * @param header: The header of the packet.
+     * @param payload: The payload of the packet.
+     */
+    struct Packet {
+        Header header;
+        Network::Buffer payload;
+
+        Packet(Network::Buffer data);
+        Packet(Header header, Network::Buffer payload);
+        Packet(Header header);
+        Packet() = default;
+
         /**
-         * @brief Flakkari Packet v1 (new packet)
+         * @brief Add payload to the packet.
          *
-         * @param header: The header of the packet.
-         * @param content: The content of the packet.
+         * @tparam T  The type of the payload to add.
+         * @param payload  The payload to add.
          */
-        struct Packet {
-            Header header;
-            Network::Buffer content;
+        template<typename T>
+        void addContent(T payload);
 
-            Packet(Network::Buffer data);
-            Packet(Header header, Network::Buffer content);
-            Packet(Header header);
-            Packet() = default;
+        /**
+         * @brief Add a fragment to the packet.
+         *
+         * @param fragment  The fragment to add.
+         */
+        void addFragment(Network::Buffer fragment);
 
-            /**
-             * @brief Add content to the packet.
-             *
-             * @tparam T  The type of the content to add.
-             * @param content  The content to add.
-             */
-            template<typename T>
-            void addContent(T content);
+        /**
+         * @brief Delete payload from the packet.
+         *
+         * @tparam T  The type of the payload to delete.
+         * @param payload  The payload to delete.
+         */
+        template<typename T>
+        void deleteContent(T payload);
 
-            /**
-             * @brief Add a fragment to the packet.
-             *
-             * @param fragment  The fragment to add.
-             */
-            void addFragment(Network::Buffer fragment);
-
-            /**
-             * @brief Delete content from the packet.
-             *
-             * @tparam T  The type of the content to delete.
-             * @param content  The content to delete.
-             */
-            template<typename T>
-            void deleteContent(T content);
-
-            /**
-             * @brief Delete a fragment from the packet.
-             *
-             * @param fragment  The fragment to delete.
-             */
-            void deleteFragment(Network::Buffer fragment);
-        };
+        /**
+         * @brief Delete a fragment from the packet.
+         *
+         * @param fragment  The fragment to delete.
+         */
+        void deleteFragment(Network::Buffer fragment);
+    };
 
     PACKED_END
 
-        void serializeHeader(Header header, Network::Buffer& buffer);
+    void serializeHeader(Header header, Network::Buffer& buffer);
 
-        void serializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2);
+    void serializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2);
 
-        void serializePacket(Packet packet, Network::Buffer& buffer);
+    void serializePacket(Packet packet, Network::Buffer& buffer);
 
-        void deserializeHeader(Network::Buffer buffer, Header& header);
+    void deserializeHeader(Network::Buffer buffer, Header& header);
 
-        void deserializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2);
+    void deserializeBuffer(Network::Buffer buffer, Network::Buffer& buffer2);
 
-        void deserializePacket(Network::Buffer buffer, Packet& packet);
+    void deserializePacket(Network::Buffer buffer, Packet& packet);
 
-    } /* namespace V_1 */
+} /* namespace V_1 */
 
 } // namespace Flakkari::Protocol::API
 
