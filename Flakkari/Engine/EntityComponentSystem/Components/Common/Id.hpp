8b00eebaf8a2397f10ecf5d0562208c074d801ca
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-13 06:51:08 +0100

    feat(Common): Add Id.hpp to Engine/EntityComponentSystem/Components/Common directory

---
+++ Flakkari/Engine/EntityComponentSystem/Components/Common/Id.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** Title: Flakkari
+** Author: MasterLaplace
+** Created: 2023-01-06
+** File description:
+** Id
+*/
+
+#ifndef ID_HPP_
+#define ID_HPP_
+
+#include <cstdint>
+
+#include "Network/Packed.hpp"
+
+namespace Engine::ECS::Components::Common {
+PACKED_START
+
+struct Id {
+    std::size_t id;
+
+    Id() : id(0) {}
+    Id(const Id &other) : id(other.id) {}
+    Id(std::size_t id) : id(id) {}
+
+    std::size_t size() const {
+        return sizeof(id);
+    }
+};
+
+PACKED_END
+} // namespace Engine::ECS::Components::Common
+
+#endif /* !ID_HPP_ */
