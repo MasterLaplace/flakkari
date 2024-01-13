b1e037345c31f08c62061e0519839773bd8ae506
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-13 01:19:45 +0100

    feat(Protocol): Add Components enum class for Flakkari Protocol

---
+++ Flakkari/Protocol/Components.hpp
@@ -0,0 +1,45 @@
+/**************************************************************************
+ * Flakkari Library v0.2.0
+ *
+ * Flakkari Library is a C++ Library for Network.
+ * @file Components.hpp
+ * @brief This file contains the Components enum class.
+ *        It is used to identify the components in the Flakkari Protocol.
+ *
+ * @see inspired by the https://en.wikipedia.org/wiki/IPv4 header
+ *
+ * Flakkari Library is under MIT License.
+ * https://opensource.org/licenses/MIT
+ * © 2023 @MasterLaplace
+ * @version 0.2.0
+ * @date 2024-01-12
+ **************************************************************************/
+
+
+#ifndef COMPONENTS_HPP_
+    #define COMPONENTS_HPP_
+
+#include <cstdint>
+
+namespace Flakkari::Protocol::API {
+
+inline namespace V_0 {
+
+    enum class ComponentId : uint8_t {
+        // 0 - 9: 2D components
+        CONTOL = 0,
+        MOVABLE = 1,
+        TRANSFORM = 2,
+        // 10 - 19: Common components
+        CHILD = 10,
+        PARENT = 11,
+        TAG = 12,
+        ID = 13,
+        TEMPLATE = 14
+    };
+
+} /* namespace V_0 */
+
+} // namespace Flakkari::Protocol::API
+
+#endif /* !COMPONENTS_HPP_ */
