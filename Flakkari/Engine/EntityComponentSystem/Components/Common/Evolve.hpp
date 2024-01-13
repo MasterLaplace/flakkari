5ce90dda06e4d297c23061b762b4d226c8428923
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-13 06:50:10 +0100

    feat(Common): Add Evolve component to ECS

---
+++ Flakkari/Engine/EntityComponentSystem/Components/Common/Evolve.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2024
+** Title: Flakkari
+** Author: MasterLaplace
+** Created: 2023-01-13
+** File description:
+** Evolve
+*/
+
+#ifndef EVOLVE_HPP_
+#define EVOLVE_HPP_
+
+#include <string>
+#include <cstring>
+
+#include "Network/Packed.hpp"
+
+namespace Flakkari::Engine::ECS::Components::Common {
+PACKED_START
+
+/**
+ * @brief Evolve component for ECS entities that can evolve in to another Component
+ *
+ * @details This component is used to evolve an entity in to another entity
+ *          based on a template
+ */
+struct Evolve {
+    const char *name;
+
+    Evolve() : name("") {}
+    Evolve(const std::string &nname) : name(nname.c_str()) {}
+    Evolve(const char *nname) : name(nname) {}
+    Evolve(const Evolve &other) : name(other.name) {}
+
+    std::size_t size() const {
+        return std::strlen(name);
+    }
+};
+
+PACKED_END
+} // namespace Flakkari::Engine::ECS::Components::Common
+
+#endif /* !EVOLVE_HPP_ */
