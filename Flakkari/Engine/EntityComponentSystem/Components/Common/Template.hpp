bcf0da4fd7784d2b7d4fbb968523b3523a8a925e
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-13 06:52:00 +0100

    feat(Common): Update Id and add Template component

---
+++ Flakkari/Engine/EntityComponentSystem/Components/Common/Template.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** Title: Flakkari
+** Author: MasterLaplace
+** Created: 2024-01-13
+** File description:
+** Template
+*/
+
+#ifndef TEMPLATE_HPP_
+#define TEMPLATE_HPP_
+
+#include <string>
+#include <cstring>
+
+#include "Network/Packed.hpp"
+
+namespace Flakkari::Engine::ECS::Components::Common {
+PACKED_START
+
+struct Template {
+    const char *name;
+
+    Template() : name("") {}
+    Template(const std::string &nname) : name(nname.c_str()) {}
+    Template(const char *nname) : name(nname) {}
+
+    std::size_t size() const {
+        return std::strlen(name);
+    }
+};
+
+PACKED_END
+} // namespace Flakkari::Engine::ECS::Components::Common
+
+#endif /* !TEMPLATE_HPP_ */
