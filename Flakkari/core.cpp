ce6d10b9ff1ab7deb8c6c54bf5582e3be246a831
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-14 08:09:31 +0100

    update(Flakkari): Update main function to accept command line arguments

--- Flakkari/core.cpp
+++ Flakkari/core.cpp
@@ -7,14 +7,18 @@
 
 #include "Server/UDPServer.hpp"
 
-int main()
+int main(int ac, const char *av[])
 {
+    if (ac != 2)
+        return FLAKKARI_LOG_FATAL("Usage: ./r-type_server <ip>"), 84;
     try {
-        Flakkari::UDPServer server("localhost", 8080);
+        Flakkari::UDPServer server(av[1], 8080);
         server.run();
     } catch (const std::exception &e) {
         if (std::string(e.what()) != "exit")
             return FLAKKARI_LOG_FATAL(e.what()), 84;
+    } catch (...) {
+        return FLAKKARI_LOG_FATAL("Unknown error"), 84;
     }
     return 0;
 }
